3adbb27199217a6e9a638561648bf9c1221f07c9
btalleman <btalleman@gmail.com>
2020-04-06 20:58:54 -0600

    Added container and item files

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                { title: 'SNF' },
+                { title: 'Quip' },
+                { title: 'Eventbrite' }
+            ]
+        };
+    }
+
+
+    portfolioItems() {
+            return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} />;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+                
+                {this.portfolioItems()}
+            </div>
+        );
+    }
+
+    
+}
